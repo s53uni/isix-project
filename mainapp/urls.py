c3b67f28e120e2f1080c1bac3cc53d9b029fae39
siyoon <s53uni@gmail.com>
2023-10-08 19:33:54 +0900

    Create Introduction Page

--- mainapp/urls.py
+++ mainapp/urls.py
@@ -4,8 +4,15 @@ from . import views
 
 urlpatterns = [
     ### 기술 소개 페이지
-    # - http://127.0.0.1:8000/intro/
-    path('intro/', views.intro),
+    # - 자동화 검사 페이지
+    # - http://127.0.0.1:8000/intro/inspection/
+    path('intro/inspection/', views.intro_inspection),
+    # - 공정 모니터링 페이지
+    # - http://127.0.0.1:8000/intro/monitoring/
+    path('intro/monitoring/', views.intro_monitoring),
+    # - 생산 계획 페이지
+    # - http://127.0.0.1:8000/intro/planning/
+    path('intro/planning/', views.intro_plannning),
     
     ### 메인 페이지
     # - http://127.0.0.1:8000/index/
--- mainapp/views.py
+++ mainapp/views.py
@@ -1,15 +1,25 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
-
-
-### intro 기술 소개 페이지
-def intro(request):
+### 기술 소개 - 자동화 검사 페이지
+def intro_inspection(request):
     return render(request,
-                  "mainapp/detail/intro.html",
+                  "mainapp/introduce/intro_inspection.html",
                   {})
 
-### main 메인 페이지
+### 기술 소개 - 공정 모니터링 페이지
+def intro_monitoring(request):
+    return render(request,
+                  "mainapp/introduce/intro_monitoring.html",
+                  {})
+
+### 기술 소개 - 생산 계획 페이지
+def intro_plannning(request):
+    return render(request,
+                  "mainapp/introduce/intro_planning.html",
+                  {})
+
+### 메인 페이지
 def main(request):
     return render(request,
                   "mainapp/main.html",
